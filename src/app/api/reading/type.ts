459d89a64afd17a3865bc63ba4311eab0dbc25a1
eespinor <edgard.espinoza.rivas@gmail.com>
2024-05-13 14:53:52 -0500

    fix: limit add id return reading request

--- src/app/api/reading/type.ts
+++ src/app/api/reading/type.ts
@@ -1,4 +1,5 @@
 export type ReadingDto = {
+  id: string;
   value: number;
   year: number;
   month: number;
@@ -9,17 +10,20 @@ export type ReadingDto = {
 };
 
 export const readingDtoFromReading = ({
+  id,
   value,
   year,
   month,
   room,
 }: {
+  id: string;
   value: number;
   year: number;
   month: number;
   room: { name: string; id: string };
 }): ReadingDto => {
   return {
+    id: id,
     value: value,
     year: year,
     month: month,
